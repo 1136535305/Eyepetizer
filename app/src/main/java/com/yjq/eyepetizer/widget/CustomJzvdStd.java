9810f9c46beb5464e32f4e04bb8516fda5e24bcd
113655305 <1136535305@qq.com>
2018-09-14 16:41:46 +0800

    视频播放页面(三)

--- app/src/main/java/com/yjq/eyepetizer/widget/CustomJzvdStd.java
+++ app/src/main/java/com/yjq/eyepetizer/widget/CustomJzvdStd.java
@@ -7,10 +7,16 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.yjq.eyepetizer.R;
 import com.yjq.eyepetizer.util.sys.ScreenUtil;
+
+import org.w3c.dom.Text;
 
 import cn.jzvd.JZDataSource;
 import cn.jzvd.JzvdStd;
@@ -24,9 +30,28 @@ import cn.jzvd.JzvdStd;
 public class CustomJzvdStd extends JzvdStd {
 
     private Context context;
-    private ImageView videoShare;
-    private ImageView videoLike;
-    private ImageView videoMoreAction;
+
+
+    //view  新增的UI组件
+    private ImageView videoShare;           //右上角【分享】功能
+    private ImageView videoLike;            //右上角【点赞】功能
+    private ImageView videoMoreAction;      //右上角【更多】功能
+    private ImageView videoMinScreen;       //全屏状态下左上角的【退出全屏】功能
+    private ImageView ivShareQQ;
+    private ImageView ivShareMoment;
+    private ImageView ivShareWeibo;
+    private ImageView ivShareWeChat;
+    private ProgressBar videoVolumeProgress;
+    private ProgressBar videoLightProgress;
+
+    private RelativeLayout rightContainer;   // 布局左侧，声音UI容器
+    private RelativeLayout leftContainer;    // 布局右侧，亮度UI容器
+    private LinearLayout shareContainer;        //分享布局
+    private LinearLayout progressContainer;
+
+    private TextView tvForwardIcon;
+    private TextView tvCurrentTime;
+    private TextView tvTotalTime;
 
     public CustomJzvdStd(Context context) {
         super(context);
@@ -51,10 +76,32 @@ public class CustomJzvdStd extends JzvdStd {
         videoLike = findViewById(R.id.video_like);
         videoShare = findViewById(R.id.video_share);
         videoMoreAction = findViewById(R.id.video_more_action);
+        videoMinScreen = findViewById(R.id.video_min_screen);
+        rightContainer = findViewById(R.id.layout_right);
+        leftContainer = findViewById(R.id.layout_left);
+        shareContainer = findViewById(R.id.layout_share);
+        progressContainer = findViewById(R.id.layout_progress);
+        videoVolumeProgress = findViewById(R.id.video_volume_bar);
+        videoLightProgress = findViewById(R.id.video_light_bar);
+        ivShareWeChat = findViewById(R.id.ivShareWechat);
+        ivShareMoment = findViewById(R.id.ivShareMoment);
+        ivShareWeibo = findViewById(R.id.ivShareWeibo);
+        ivShareQQ = findViewById(R.id.ivShareQQ);
+
+        tvForwardIcon = findViewById(R.id.tvForward);
+        tvCurrentTime = findViewById(R.id.tvCurrentTime);
+        tvTotalTime = findViewById(R.id.tvTotalTime);
+
 
         videoLike.setOnClickListener(this);
         videoShare.setOnClickListener(this);
         videoMoreAction.setOnClickListener(this);
+        videoMinScreen.setOnClickListener(this);
+        ivShareWeChat.setOnClickListener(this);
+        ivShareMoment.setOnClickListener(this);
+        ivShareWeibo.setOnClickListener(this);
+        ivShareQQ.setOnClickListener(this);
+
     }
 
 
@@ -69,16 +116,34 @@ public class CustomJzvdStd extends JzvdStd {
         FULLSCREEN_ORIENTATION = ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE;
 
 
-        //非全屏状态下的变化
-        if (currentScreen == SCREEN_WINDOW_NORMAL || currentScreen == SCREEN_WINDOW_LIST) {
-            backButton.setVisibility(View.VISIBLE);                            //回退按钮R.id.back 可见
-            titleTextView.setVisibility(View.GONE);                            //不显示视频标题R.id.title
-            fullscreenButton.setImageResource(R.mipmap.ic_action_full_screen); //修改图标背景图片R.id.fullscreen
-        }
+        //重置4种状态下UI组件的视觉变化
+        if (currentScreen == SCREEN_WINDOW_FULLSCREEN) {
+            backButton.setVisibility(View.GONE);
+            fullscreenButton.setVisibility(INVISIBLE);
+            batteryTimeLayout.setVisibility(View.GONE);
+            videoMinScreen.setVisibility(View.VISIBLE);
+            shareContainer.setVisibility(VISIBLE);
+            rightContainer.setVisibility(VISIBLE);
+            leftContainer.setVisibility(VISIBLE);
+            progressContainer.setVisibility(VISIBLE);
 
+        } else if (currentScreen == SCREEN_WINDOW_NORMAL || currentScreen == SCREEN_WINDOW_LIST) {    //正常状态下
+            backButton.setVisibility(View.VISIBLE);                                                   //回退按钮R.id.back 可见
+            titleTextView.setVisibility(View.GONE);                                                   //不显示视频标题R.id.title
+            videoMinScreen.setVisibility(View.GONE);
+            batteryTimeLayout.setVisibility(View.GONE);
+            fullscreenButton.setVisibility(VISIBLE);
+            rightContainer.setVisibility(GONE);
+            leftContainer.setVisibility(GONE);
+            shareContainer.setVisibility(GONE);
+            progressContainer.setVisibility(GONE);
+            fullscreenButton.setImageResource(R.mipmap.ic_action_full_screen);                        //修改图标背景图片R.id.fullscreen
+        }
 
     }
 
+
+    //所有点击事件的处理
     @Override
     public void onClick(View v) {
         super.onClick(v);
@@ -89,6 +154,7 @@ public class CustomJzvdStd extends JzvdStd {
         }
 
 
+        //新增UI组件的点击事件
         switch (v.getId()) {
             case R.id.video_like:
                 Toast.makeText(getContext(), "like", Toast.LENGTH_SHORT).show();
@@ -99,6 +165,25 @@ public class CustomJzvdStd extends JzvdStd {
             case R.id.video_more_action:
                 Toast.makeText(getContext(), "more_action", Toast.LENGTH_SHORT).show();
                 break;
+            case R.id.ivShareMoment:
+                Toast.makeText(getContext(), "ivShareMoment", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ivShareQQ:
+                Toast.makeText(getContext(), "ivShareQQ", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ivShareWechat:
+                Toast.makeText(getContext(), "ivShareWechat", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ivShareWeibo:
+                Toast.makeText(getContext(), "ivShareWeibo", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.video_min_screen:
+                if (currentState == CURRENT_STATE_AUTO_COMPLETE) return;
+                if (currentScreen == SCREEN_WINDOW_FULLSCREEN) {
+                    backPress();
+                }  //quit fullscreen
+                break;
+
         }
     }
 
@@ -116,7 +201,7 @@ public class CustomJzvdStd extends JzvdStd {
         } else if (currentState == CURRENT_STATE_AUTO_COMPLETE) {
             startButton.setVisibility(VISIBLE);
             startButton.setImageResource(R.mipmap.ic_player_reload);
-            replayTextView.setVisibility(VISIBLE);
+            replayTextView.setVisibility(GONE);
         } else {
             startButton.setImageResource(R.mipmap.ic_player_play);
             replayTextView.setVisibility(INVISIBLE);
@@ -127,11 +212,97 @@ public class CustomJzvdStd extends JzvdStd {
     //修改播放按钮大小
     @Override
     public void changeStartButtonSize(int size) {
-        size = ScreenUtil.INSTANCE.dip2px(80);
+        size = ScreenUtil.INSTANCE.dip2px(70);
         ViewGroup.LayoutParams lp = startButton.getLayoutParams();
         lp.height = size;
         lp.width = size;
     }
 
 
+    //控制新增UI部分的显隐
+    @Override
+    public void setAllControlsVisiblity(int topCon, int bottomCon, int startBtn, int loadingPro,
+                                        int thumbImg, int bottomPro, int retryLayout) {
+        super.setAllControlsVisiblity(topCon, bottomCon, startBtn, loadingPro, thumbImg, bottomPro, retryLayout);
+
+
+        progressContainer.setVisibility(GONE);
+        if (currentScreen == SCREEN_WINDOW_FULLSCREEN) {
+            rightContainer.setVisibility(topCon);
+            leftContainer.setVisibility(topCon);
+            shareContainer.setVisibility(topCon);
+
+        }
+    }
+
+
+    //一定时间内不点击屏幕自动隐藏UI任务
+    @Override
+    public void dissmissControlView() {
+        if (currentState != CURRENT_STATE_NORMAL
+                && currentState != CURRENT_STATE_ERROR
+                && currentState != CURRENT_STATE_AUTO_COMPLETE) {
+            post(new Runnable() {
+                @Override
+                public void run() {
+                    shareContainer.setVisibility(View.INVISIBLE);
+                    progressContainer.setVisibility(View.INVISIBLE);
+                    rightContainer.setVisibility(View.INVISIBLE);
+                    leftContainer.setVisibility(View.INVISIBLE);
+                    bottomContainer.setVisibility(View.INVISIBLE);
+                    topContainer.setVisibility(View.INVISIBLE);
+                    startButton.setVisibility(View.INVISIBLE);
+                    if (clarityPopWindow != null) {
+                        clarityPopWindow.dismiss();
+                    }
+                    if (currentScreen != SCREEN_WINDOW_TINY) {
+                        bottomProgressBar.setVisibility(View.VISIBLE);
+                    }
+                }
+            });
+        }
+    }
+
+
+    //调整亮度时UI效果
+    @Override
+    public void showBrightnessDialog(int brightnessPercent) {
+
+        leftContainer.setVisibility(VISIBLE);
+        if (brightnessPercent > 100) {
+            brightnessPercent = 100;
+        } else if (brightnessPercent < 0) {
+            brightnessPercent = 0;
+        }
+        videoLightProgress.setProgress(brightnessPercent);
+        onCLickUiToggleToClear();
+    }
+
+
+    //调整声音时UI效果
+    @Override
+    public void showVolumeDialog(float deltaY, int volumePercent) {
+        rightContainer.setVisibility(VISIBLE);
+        if (volumePercent > 100) {
+            volumePercent = 100;
+        } else if (volumePercent < 0) {
+            volumePercent = 0;
+        }
+        videoVolumeProgress.setProgress(volumePercent);
+        onCLickUiToggleToClear();
+    }
+
+
+    @Override
+    public void showProgressDialog(float deltaX, String seekTime, long seekTimePosition, String totalTime, long totalTimeDuration) {
+        progressContainer.setVisibility(VISIBLE);
+        tvCurrentTime.setText(seekTime);
+        tvTotalTime.setText(" / " + totalTime);
+        if (deltaX > 0) {
+            tvForwardIcon.setText(R.string.fast_forward);
+        } else {
+            tvForwardIcon.setText(R.string.rewind);
+        }
+        onCLickUiToggleToClear();
+    }
 }
