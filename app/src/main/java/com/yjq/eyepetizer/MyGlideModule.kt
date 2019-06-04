4cef25dba5fa8500ecbfdebdd2acb4aafc85be61
YJQ <1136535305@qq.com>
2019-06-04 20:32:32 +0800

    1.移除DataBinding的kapt引用（Android studio 3.2.0-beta01开始，DataBinding不再需要在gradle文件中声明其注释处理器依赖项，因为数据绑定能够解析其依赖性）； 2.Android Studio升级到3.4，相应地升级了Gradle和Gradle插件的版本； 3.Kotlin版本升级带来的一些配置configuration的细微改动；

--- app/src/main/java/com/yjq/eyepetizer/MyGlideModule.kt
+++ app/src/main/java/com/yjq/eyepetizer/MyGlideModule.kt
@@ -1,13 +1,14 @@
 package com.yjq.eyepetizer
 
 /**
- * 文件： AppGlideModule
+ * 文件： MyGlideModule
  * 描述： 使用Glide 4.0版本以上一些方法需要写的类
  * 作者： YangJunQuan   2017/12/26.
  */
 
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 
 @GlideModule
-class AppGlideModule : com.bumptech.glide.module.AppGlideModule()
+class MyGlideModule : AppGlideModule()
 
